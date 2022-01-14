f0090b9540f359a7119a23d631434c0ce0da5282
Shawn Lentz <shawn.lentz@gmail.com>
2022-01-14 12:12:31 -1000

    Got images to work on cards

--- src/App.js
+++ src/App.js
@@ -1,23 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
+// import fs from 'fs';
+// import fs from 'fs';
+import React from 'react';
+import Image from './components/images/75.png'; // Need to get all the images from the folder
+import style from './styles/cards.css';
+import Card from './components/card.js';
+import getImages from './components/get-images.js';
+import uniqid from 'uniqid';
+
+// Reader stack overflow https://stackoverflow.com/questions/57161839/module-not-found-error-cant-resolve-fs-in
+// const fs = require('fs')
 
 function App() {
+  const imageList = getImages()
+  const images = require.context('./components/images', true)
+  // const luigi = images('./1.png')
+  // console.log("IMAGES", images)
+  function listFiles() {
+    for(let i = 0; i < 151; i++) {
+      try {
+        console.log(`./components/images/${i}.png`)
+      } catch {
+        console.log("No such file exists")
+      }
+    }
+  }
   return (
+
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {/* <Card image={'./components/images/75.png'}/> */}
+      {imageList.map((img, i) => {
+        try {
+          let newImage =  images(img)
+          return <Card image={newImage} key={uniqid()} />
+        } catch {
+          console.log("No such image")
+        }
+        // newImage.src = img
+        // return <Card image={newImage} key={uniqid()} />
+        // <Card />
+      })}
+      {/* <div className="card">
+        <img src={Image}/> */}
+        {/* <Card /> */}
     </div>
   );
 }
